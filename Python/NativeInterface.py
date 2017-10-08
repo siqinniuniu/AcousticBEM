a8cfb7f71afac1ce0e79499db58e55db7878652b
Guillaume Jacquenot <guillaume.jacquenot@gmail.com>
2017-10-08 15:55:57 +0200

    Updated so path

--- Python/NativeInterface.py
+++ Python/NativeInterface.py
@@ -16,19 +16,24 @@
 # along with AcousticBEM.  If not, see <http://www.gnu.org/licenses/>.
 # ---------------------------------------------------------------------------
 from ctypes import *
+import os
 
-helmholtz = CDLL("/home/fjargsto/AcousticBEM/C/libhelmholtz.so")
+libraryPath = os.path.join(os.path.dirname(__file__), '..', 'C', 'libhelmholtz.so')
+
+# "/home/fjargsto/AcousticBEM/C/libhelmholtz.so"
+
+helmholtz = CDLL(libraryPath)
 
 helmholtz.Hankel1.argtypes = [c_int, c_float, c_void_p]
 
 
 class Complex(Structure):
     _fields_ = [('re', c_float), ('im', c_float)]
-    
+
 class Float2(Structure):
     _fields_ = [('x', c_float), ('y', c_float)]
-    
+
 class Float3(Structure):
     _fields_ = [('x', c_float), ('y', c_float), ('z', c_float)]
-    
-    
+
+
