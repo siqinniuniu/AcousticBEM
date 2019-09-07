f5f07f3493220550136d8516465a73bc64cb7633
Frank Jargstorff <fjargsto@gmail.com>
2019-09-07 13:06:58 -0700

    adding 2.7 and 3.7 support

--- abem/setup.py
+++ abem/setup.py
@@ -1,33 +1,37 @@
 from setuptools import setup, Extension
 
-
 intops = Extension('intops',
                    sources=['intops/intops.c'],
                    depends=['intops/intops.h'],
-                   language='c',)
-
+                   language='c', )
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
-    setup(name='abem',
-          version='1.0a0',
-          description='Boundary Element Method for Acoustic Simulations',
-          long_description=long_description,
-          url='http://github.com/fjargsto/AcousticBEM',
-          author='Frank Jargstorff',
-          license='GNU General Public License',
-          packages=['abem'],
-          zip_safe=False,
-          ext_modules=[intops],
-          test_suite='test',
-          data_files=[('notebooks', ['notebooks/exterior_helmholtz_solver_2d.ipynb',
-                                     'notebooks/exterior_helmholtz_solver_3d.ipynb',
-                                     'notebooks/exterior_helmholtz_solver_rad.ipynb',
-                                     'notebooks/interior_helmholtz_solver_2d.ipynb',
-                                     'notebooks/interior_helmholtz_solver_3d.ipynb',
-                                     'notebooks/interior_helmholtz_solver_rad.ipynb',
-                                     'notebooks/rayleigh_cavity_1.ipynb',
-                                     'notebooks/rayleigh_cavity_2.ipynb',
-                                     'notebooks/rayleigh_solver_3d_disk.ipynb',
-                                     'notebooks/rayleigh_solver_square.ipynb'])],
-          )
+
+setup(
+    name='abem',
+    version='0.0a0',
+    description='Boundary Element Method for Acoustic Simulations',
+    long_description=long_description,
+    url='http://github.com/fjargsto/AcousticBEM',
+    author='Frank Jargstorff',
+    license='GNU General Public License',
+    packages=['abem'],
+    install_requires=['numpy', 'scipy'],
+    zip_safe=False,
+    ext_modules=[intops],
+    test_suite='test',
+    data_files=[(
+        'notebooks', [
+            'notebooks/exterior_helmholtz_solver_2d.ipynb',
+            'notebooks/exterior_helmholtz_solver_3d.ipynb',
+            'notebooks/exterior_helmholtz_solver_rad.ipynb',
+            'notebooks/interior_helmholtz_solver_2d.ipynb',
+            'notebooks/interior_helmholtz_solver_3d.ipynb',
+            'notebooks/interior_helmholtz_solver_rad.ipynb',
+            'notebooks/rayleigh_cavity_1.ipynb',
+            'notebooks/rayleigh_cavity_2.ipynb',
+            'notebooks/rayleigh_solver_3d_disk.ipynb',
+            'notebooks/rayleigh_solver_square.ipynb']
+    )],
+)
