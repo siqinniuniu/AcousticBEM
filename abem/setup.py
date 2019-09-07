c1bc4ba19c7ad6c297e7a7b1d269b08606b6b060
Frank Jargstorff <fjargsto@gmail.com>
2019-09-07 15:54:55 -0700

    bumping version, fixing markdown rendering bug

--- abem/setup.py
+++ abem/setup.py
@@ -10,9 +10,10 @@ with open("README.md", "r") as fh:
 
 setup(
     name='abem',
-    version='0.0a0',
+    version='0.0a1',
     description='Boundary Element Method for Acoustic Simulations',
     long_description=long_description,
+    long_description_content_type='text/markdown',
     url='http://github.com/fjargsto/AcousticBEM',
     author='Frank Jargstorff',
     license='GNU General Public License',
